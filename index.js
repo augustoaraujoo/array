cc48aca6e4e72411c546afd2e17477f8a7c60768
Augusto Araujo <augustinho_darocha1311@hotmail.com>
2021-06-01 19:57:03 -0300

    ajuste js

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-const array = [0,1,2,3,4,5]
-//apenas visualizando o arary
-// console.log(`meus arrays = ${array}`)
+    const array = [0,1,2,3,4,5]
+     //apenas visualizando o arary
+        // console.log(`meus arrays = ${array}`)
 
 
-const array2 = [0,1,2,3,4,5]
-//adicionando o valor "66" no array2
-array2.push(66)
-console.log(array2)
+    const array2 = [0,1,2,3,4,5]
+        //adicionando o valor "66" no array2
+            array2.push(66)
+                console.log(array2)
 
-const array3 = [0,1,2,3,4,5]
-//buscando o número 3 e trocando ele por "6"
-array3[3]=(6)
-console.log(array3)
+    const array3 = [0,1,2,3,4,5]
+     //buscando o número 3 e trocando ele por "6"
+         array3[3]=(6)
+             console.log(array3)
 
-array4 = [1,2,3,4,5]
-/*mostra quantos números tem no array4*/
-console.log(array4.length)
+   const array4 = [1,2,3,4,5]
+         /*mostra quantos números tem no array4*/
+             console.log(array4.length)
 
-const array5 ={
-    nome:'userAll',
-    color: 'blue'
-}
-console.log(Object.entries(array5))
+    const array5 ={
+            nome:'userAll',
+             color: 'blue'
+    }
+    console.log(Object.entries(array5))
+    // Object.values || Object.keys || Object.freeze \\
 
-
+    const array6= ['b','a','d','e','f','c']
+        //organiza o array a,b,c,d,e,f
+            array6.sort()
+                console.log(array6)
+            
+    const array7 = ['joâo','pedro','lucas']
+    array7.pop()//remove o último elemento
+    array7.shift()//remove o primeiro elemento
+    array7.unshift('user')//adiciona na indice "0"
