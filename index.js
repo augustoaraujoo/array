652ae9ce671308f277a9ea0048e0bbd1f8ce7d62
Augusto Araujo <augustinho_darocha1311@hotmail.com>
2021-06-01 19:26:50 -0300

    array

---
+++ index.js
@@ -0,0 +1,26 @@
+const array = [0,1,2,3,4,5]
+//apenas visualizando o arary
+// console.log(`meus arrays = ${array}`)
+
+
+const array2 = [0,1,2,3,4,5]
+//adicionando o valor "66" no array2
+array2.push(66)
+console.log(array2)
+
+const array3 = [0,1,2,3,4,5]
+//buscando o número 3 e trocando ele por "6"
+array3[3]=(6)
+console.log(array3)
+
+array4 = [1,2,3,4,5]
+/*mostra quantos números tem no array4*/
+console.log(array4.length)
+
+const array5 ={
+    nome:'userAll',
+    color: 'blue'
+}
+console.log(Object.entries(array5))
+
+
